bcbd3c2a03d08d9cd9eff68f41abb3c03cca3623
chrisL <chrisrlawrence2005@outlook.com>
2025-07-21 11:26:34 +1200

    implemented milliseconds ISR and timer

--- main.c
+++ main.c
@@ -53,61 +53,93 @@ const int cal_max = 1023;
 // Initialise timer for microsecond counting
 void initMicroTimer() {
 
-    // Configure Clock Registers
-    __bis_SR_register(SCG0);                //Disable FLL
-    CSCTL1 = DCORSEL_0;                          // Set SMCLK to 1 MHz
-    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;        // No dividers
-    __delay_cycles(3);                   // Small delay to allow time for reconfiguration
-    __bic_SR_register(SCG0);               //Enable FLL
+    __bis_SR_register(SCG0);                   // disable FLL
+    CSCTL3 |= SELREF__REFOCLK;                      // Set REFO as FLL reference source
+    CSCTL0 = 0;                                     // clear DCO and MOD registers
+    CSCTL1 &= ~(DCORSEL_7);                         // Clear DCO frequency select bits first
+    CSCTL1 |= DCORSEL_2;                            // Set DCO = 4MHz
+    CSCTL2 = FLLD_1 + 60;                           // DCODIV = 2MHz
+    __delay_cycles(3);                          
+    __bic_SR_register(SCG0);                   // enable FLL
+
+    while(CSCTL7 & (FLLUNLOCK0 | FLLUNLOCK1));      // Poll until FLL is locked
+
+    CSCTL4 = SELMS__DCOCLKDIV | SELA__REFOCLK;      // set default REFO(~32768Hz) as ACLK source, ACLK = 32768Hz
+                                                    // default DCODIV as MCLK and SMCLK source
+    CSCTL5 |= DIVM__1 | DIVS__2;                    // SMCLK = 1MHz, MCLK = 2MHz
 
     // Timer B0 setup: continuous mode, SMCLK source, no divider
     TB0CTL = TBSSEL_2 | MC_2 | TBCLR | TBIE; // SMCLK, Continuous mode, Clear, Interrupt enable
 }
 
-#pragma vector = TIMER0_B0_VECTOR
+void initMilliTimer() {
+
+    // Setup Timer 
+    TB3CCR0 = TIMER_1MS_SMCLK -1;               // 1ms interval
+    TB3CCTL0 = CCIE;                            // Enable interrupt for CCR0
+    TB3CTL = TBSSEL_2 | MC_1 | TBCLR;           // use SMCLK, count up to TB0CCR0, and clear timer
+
+}
+
+// Microsecond ISR
+#pragma vector = TIMER0_B1_VECTOR
 __interrupt void Timer_B_ISR(void) {
 
-    // If the pending interrupt is an overflow flag, increment overflows
-    if (TB0IV == TBIV__TBIFG) {
-        timer_overflows++;
+    // Switch statements to avoid undefined behaviour for other interrupt flags
+    switch (TB0IV) {
 
-        if (waiting_for_interrupt == 1) {
+        // Overflow flag case
+        case TBIV__TBIFG:
 
-            // Combine overflows and TB0R into 32-bit timestamp
-            uint32_t current_time = (timer_overflows << 16) | TB0R;
-            total_deadtime += (current_time - waiting_t1);
-        }
+            timer_overflows++; // Increment microseconds timer
 
-        waiting_for_interrupt = 0;
+            // Increment system deadtime
+            if (waiting_for_interrupt == 1) {
+                uint32_t current_time = (timer_overflows << 16) | TB0R;
+                total_deadtime += (current_time - waiting_t1);
+            }
 
-    }
+            // Reset interrupt flag
+            waiting_for_interrupt = 0;
+            break;
+
+        // Invalid case handling
+        default:
+            break;
+}
+}
+
+// Millisecond ISR
+#pragma vector = TIMER3_B0_VECTOR
+__interrupt void TimerB3ISR(void) {
+
+    // Increment ms counter
+    interrupt_timer++;
+
+}
+
+// Function to return milliseconds since inception
+// Will overflow after approximately 49.7 days (2^32ms)
+uint32_t millis() {
+    return interrupt_timer;
 }
 
 // Function to get a 32-bit timer count combining timer and overflow count
 // Overflow will occur every 71 minutes @ 1 MHz, Depending on timestamp requirements will need to find way to manage
 uint32_t micros() {
 
-    // Variables to hold successive readings of the timer register and overflow counter
-    uint16_t t1, t2;
+    // Variables to hold successive readings of the overflow counter and timer register
+    uint16_t t1;
     uint32_t ovf1, ovf2;
 
     // Loop until consistent readings are obtained, this handles the case where the timer overflows during the read
     do {
-
-        // Take timer register and overflow counter readings
         ovf1 = timer_overflows;
         t1 = TB0R;
         ovf2 = timer_overflows;
-        t2 = TB0R;
+    } while (ovf1 != ovf2);
 
-    } while (ovf1 != ovf2 || t2 < t1); // Check for overflow during read
-
-    return (ovf2 << 16) | t2; // Combine overflow count and timer value
-}
-
-// Placeholder function for returning milliseconds since program start
-unsigned long millis(){
-   return micros() / 1000; 
+    return (ovf2 << 16) | t1; // Combine overflow count and timer value
 }
 
 // Placeholder function for marking the start of a timed interval
@@ -278,8 +310,11 @@ int main(void){
     ADCCTL2 = ADCRES_2;                                         // 12-bit resolution ** TODO: find appropriate clock frequency for bit resolution **
     ADCMCTL0 = ADCINCH_0;                                       // Default to channel 0 (A0)
 
-    // // Timer B0 Setup for system timer
+    // Timer B0 Setup for system timer (microseconds)
     initMicroTimer();
+
+    // Timer B3 setup for milliseconds timer 
+    initMilliTimer();
 
     // Timer B1 Setup for LED PWM
     TB1CCR0 = TIMER_1MS_SMCLK -1;               // 1ms interval
