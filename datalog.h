a0aada47461dd53ddfd726fcc4b4ac643cb386ef
chrisL <chrisrlawrence2005@outlook.com>
2025-09-02 13:29:49 +1200

    feat(datalog): Add initial datalogging header setup

---
+++ datalog.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+*
+* Author: Chris Lawrence
+*
+* Summary: Header file for data logging operations
+* 
+******************************************************************************/
+
+#ifndef DATALOG_H
+#define DATALOG_H
+
+// --- CONSTANTS ---
+#define MAX_SIZE 100        // Max array size of queue
+
+// Function Prototypes
+int isFull();
+int isEmpty();
+void enqueue(int data);
+int dequeue();
+
+#endif /* DATALOG_H */
